257c16008272b1ac9d5702506fe2ac67c6449b32
Sarhamon <dksk1910@gmail.com>
2026-05-16 16:32:03 +0900

    new file:   .gitattributes 	new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/com/framework/subcriptions/SubcriptionsApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/framework/subcriptions/SubcriptionsApplicationTests.java

---
+++ src/main/java/com/framework/subcriptions/SubcriptionsApplication.java
@@ -0,0 +1,13 @@
+package com.framework.subcriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubcriptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubcriptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/framework/subcriptions/SubcriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.subcriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubcriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
